e3bb565c1533cfa66038a4fe105dd0068b90179d
wmik <williemik.wmik@gmail.com>
2019-01-07 17:50:21 +0300

    style: fix lint errors

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
-import test from "ava";
-import pkg from "this";
+import test from 'ava';
+import pkg from '.';
 
-test("pkg name", t => {
-	t.is(pkg.name, "cjs");
+test('pkg version', t => {
+	t.is(pkg.version, '0.0.0-development');
 });
